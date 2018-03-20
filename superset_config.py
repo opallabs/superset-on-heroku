93399d65b3b1c97ec6765edc21532f10466e40eb
Dan Barrett <dbarrett83@gmail.com>
2018-03-20 11:57:15 -0700

    fixing stuff

--- superset_config.py
+++ superset_config.py
@@ -3,7 +3,7 @@ import os
 # Superset specific config
 #---------------------------------------------------------
 # ROW_LIMIT = 5000
-SUPERSET_WORKERS = 8 # for it to work in heroku basic/hobby dynos increase as you like
+SUPERSET_WORKERS = 16 # for it to work in heroku basic/hobby dynos increase as you like
 SUPERSET_WEBSERVER_PORT = os.environ['PORT']
 #---------------------------------------------------------
 MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY')
@@ -21,12 +21,25 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # you want to explore are managed directly in the web UI
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
+
+CACHE_CONFIG = {
+'CACHE_TYPE': 'redis',
+'CACHE_KEY_PREFIX': 'superset_cache',
+'CACHE_REDIS_HOST': 'ec2-54-174-198-121.compute-1.amazonaws.com',
+'CACHE_REDIS_PORT': 7819,
+'CACHE_REDIS_DB': 0,
+'CACHE_REDIS_URL': 'redis://h:pd9fe08e309449296cda1d76d4bc6d6e0ae24165adf28914141375c49016d2a23@ec2-54-174-198-121.compute-1.amazonaws.com:7819'
+}
+
+RESULTS_BACKEND = RedisCache(
+    host='h:pd9fe08e309449296cda1d76d4bc6d6e0ae24165adf28914141375c49016d2a23@ec2-54-174-198-121.compute-1.amazonaws.com', port=7819, key_prefix='superset_results')
+
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = CSRF_ENABLED = True
 
 # use inserted X-Forwarded-For/X-Forwarded-Proto headers
 ENABLE_PROXY_FIX = True
-SQLLAB_ASYNC_TIME_LIMIT_SEC = 120
-SQLLAB_TIMEOUT = 120
-SUPERSET_WEBSERVER_TIMEOUT = 120
+SQLLAB_ASYNC_TIME_LIMIT_SEC = 1000
+SQLLAB_TIMEOUT = 1000
+SUPERSET_WEBSERVER_TIMEOUT = 1000
 
